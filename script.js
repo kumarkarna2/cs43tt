1a38f781f5a90986ea1a84dac772332a52f7cc32
Karna Kumar Chaudhary <67538142+kumarkarna2@users.noreply.github.com>
2022-04-19 23:07:05 +0530

    .

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ function getDayName(dateStr, locale) {
     return date.toLocaleDateString(locale, { weekday: "long" });
 }
 
-<<<<<<< HEAD
-if (screen.width <= 650) {
-=======
 if (screen.width <= 720) {
->>>>>>> 75d146fa4b7ee1de6bc7601ba26c81cbac2c1b66
     var now = new Date();
     var dateStr = now;
     var day = getDayName(dateStr, "en-US");
@@ -63,22 +59,12 @@ fetch("./data.json")
             var day = document.getElementById(dayname);
             for (var j = 0; j < data.days[i].subjects.length; j++) {
                 day.innerHTML +=
-<<<<<<< HEAD
-                    '<a href="' +
-                    data.days[i].subjects[j].link +
-                    '" target="_blank"><div class="period"><h2>' +
-                    data.days[i].subjects[j].name +
-                    "<br>" +
-                    data.days[i].subjects[j].time +
-                    "</h2></div></a>";
-=======
                     data.days[i].subjects[j].link +
                     '" <div class="period"><h2>' +
                     data.days[i].subjects[j].name +
                     "</h2><p>";
                 data.days[i].subjects[j].time + "</p><h3></h3>";
                 ("</div>");
->>>>>>> 75d146fa4b7ee1de6bc7601ba26c81cbac2c1b66
             }
         }
     });
